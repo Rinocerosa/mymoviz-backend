16944919dd0ffb745ad8dc4a745b691698800908
rinocerosa <anaischaouati@gmail.com>
2024-04-24 16:25:35 +0200

    jj

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+router.get("/movies", (req, res) => {
+  const API_KEY = process.env.API_KEY;
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
+  console.log(process.env);
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ movies: data });
+      console.log(data);
+    });
+});
+
+module.exports = router;
